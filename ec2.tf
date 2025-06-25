aa42a70355a079060fcfdc3974c366786fc4a83a
shravan-1412 <shravan.ch6@gmail.com>
2025-06-25 18:03:54 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
-resource "aws_instance" "this" {
+resource "aws_instance" "roboshop" {
   ami           = var.ami_id
   instance_type = var.instance_type
   vpc_security_group_ids = var.sg_ids
 
-  tags = var.tags
+  tags = "hello world"
 }
